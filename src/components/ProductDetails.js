b1b824f0e810172b75af346fb03ad3f7afb0cd7c
Ali Rezaei <im1.bitcode@gmail.com>
2022-10-24 14:04:24 +0330

    update react-router-dom to version 6

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,32 +1,43 @@
-import React, {useContext} from "react";
-import {Link, useParams} from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+// import axios from "axios";
 
 // Context
-import {ProductsContext} from "../context/ProductContextProvider";
+import { ProductsContext } from "../context/ProductContextProvider";
+
+// Components
+// import { BASE_URL } from "../services/api";
 
 // Styles
 import styles from "./ProductDetails.module.css";
 
 const ProductDetails = () => {
-    const {id} = useParams()
+  //   useEffect(() => {
+  //     const [data, setData] = useState([]);
+  //     axios.get(`${BASE_URL}/products/${id}`).then((response) => setData(response.data));
+  //   }, []);
 
-    const data = useContext(ProductsContext);
-    const product = data[(id - 1)];
-    const { title, price, description, category, image } = product;
+  const { id } = useParams();
 
-    return (
-        <div className={styles.container}>
-            <img className={styles.image} src={image} alt="product"/>
-            <div className={styles.textContainer}>
-                <h3>{title}</h3>
-                <p className={styles.description}>{description}</p>
-                <p className={styles.category}><span>Category:</span> {category}</p>
-                <div className={styles.buttonContainer}>
-                    <span className={styles.price}>{price}$</span>
-                    <Link to="/products">Back to shop</Link>
-                </div>
-            </div>
+  const data = useContext(ProductsContext);
+  const product = data[id - 1];
+  const { title, price, description, category, image } = product;
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={image} alt="product" />
+      <div className={styles.textContainer}>
+        <h3>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        <p className={styles.category}>
+          <span>Category:</span> {category}
+        </p>
+        <div className={styles.buttonContainer}>
+          <span className={styles.price}>{price}$</span>
+          <Link to="/products">Back to shop</Link>
         </div>
-    )
-}
-export default ProductDetails
+      </div>
+    </div>
+  );
+};
+export default ProductDetails;
